fb109990cbc86ed17486dcceb24ed033f84d6bf5
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-08-19 19:12:54 +0530

    commitfile

---
+++ src/Coin.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Coin.css'
+function Coin({ name,image,symbol,price,volume ,priceChange, cap}) {
+   return(
+<div className="coin-container">
+   <div className="coin-row">
+      <div className="coin">
+        <img src={image} alt="cryto"/>
+        <h1 className="coin-name">{name}</h1>
+        <p className="coin-symbol">{symbol}</p>
+     </div>
+
+     <div className="coin-data">
+         <p className="coin-price">INR {price} </p>
+       
+       {priceChange<0 ? (
+         <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+       )  : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)}
+      </div>
+   </div> 
+</div>
+   );
+};
+
+export default Coin
