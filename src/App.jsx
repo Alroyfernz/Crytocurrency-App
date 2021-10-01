6eeaa316d429820718eb6db7b4b24a4bb272ecfc
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-10-01 15:51:22 +0530

    added responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,55 @@
-import React ,{useEffect, useState}from 'react'
-import './App.css';
-import axios from 'axios'
-import Coin from './Coin';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Coin from "./Coin";
 
 function App() {
-  const [coins,setCoins]=useState([]);
-  const [search,setSearch]=useState('');
-  useEffect(()=>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res=>{
-      setCoins(res.data);
-      console.log(coins);
-    })
-    .catch(error => console.log(error))
-  },[]);
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(coins);
+      })
+      .catch((error) => console.log(error));
+  }, [coins]);
 
-  function handleChange(event){
+  function handleChange(event) {
     setSearch(event.target.value);
   }
-  const filteredCoins=coins.filter(coin =>
-coin.name.toLowerCase().includes(search.toLowerCase())
-  )
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <h1 className="coin-text">
-          Search a currency
-        </h1>
+        <h1 className="coin-text">Search a currency</h1>
         <form>
-          <input type="text" placeholder="Search"  className="coin-input" onChange={handleChange}/>
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
         </form>
       </div>
-{filteredCoins.map(coin=>{
- return <Coin 
-  key={coin.id}
-    name={coin.name}
-    image={coin.image}
-    symbol={coin.symbol}
-    volume={coin.market_cap}
-    price={coin.current_price}
-    priceChange={coin.price_change_percentage_24h}
-   
-  />;
-  
-})}
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
     </div>
   );
 }
